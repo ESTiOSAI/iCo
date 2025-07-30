be47298929bef172e4fba1f77cc4e8de377d7ef0
kanghun1121 <kanghun1121@naver.com>
2025-07-30 17:18:12 +0900

    feat: NetworkClient 추가

--- AIProject/AIProject/Core/Remote/NetworkClient.swift
+++ AIProject/AIProject/Core/Remote/NetworkClient.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+final class NetworkClient {
+    func request<T: Decodable>(url: URL) async throws -> T {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let modelData = try JSONDecoder().decode(T.self, from: data)
+        return modelData
+    }
+}
