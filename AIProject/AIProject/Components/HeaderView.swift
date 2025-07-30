4a9f6a07c403eb022e4d747e83d03a4c844d15b5
kitcat-s <kitcat92.seo@gmail.com>
2025-07-30 23:00:19 +0900

    feat: 공통으로 사용할 헤더뷰 + 서브헤더뷰 구현하기

---
+++ AIProject/AIProject/Components/HeaderView.swift
@@ -0,0 +1,71 @@
+//
+//  HeaderView.swift
+//  AIProject
+//
+//  Created by Kitcat Seo on 7/30/25.
+//
+
+import SwiftUI
+
+/// 헤더에 표시할 제목을 필수로 전달해주세요.
+/// 마켓이나 북마크 메뉴일 경우 각 파라메터에 true 값을 넣어주세요.
+struct HeaderView: View {
+    @State var heading: String
+    
+    @State var showSearchButton = false
+    @State var isBookmarkView = false
+    
+    var body: some View {
+        HStack {
+            Text(heading)
+                .font(.system(size: 24, weight: .black))
+                .foregroundStyle(.aiCoLabel)
+            
+            Spacer()
+            
+            if showSearchButton {
+                Button {
+                    print("Hi")
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24)
+                        .fontWeight(.medium)
+                        .foregroundStyle(.aiCoLabel)
+                }
+            } else if isBookmarkView {
+                // 북마크 메뉴라면 북마크 관리 버튼 보여주기
+                HStack(spacing: 8) {
+                    Group {
+                        Button {
+                            // 가져오기 기능 구현하기
+                        } label: {
+                            Text("가져오기")
+                        }
+                        
+                        Button {
+                            // 내보내기 기능 구현하기
+                        } label: {
+                            Text("내보내기")
+                        }
+                    }
+                    .font(.system(size: 12))
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 6)
+                    .foregroundStyle(.aiCoLabel)
+                    .background(.aiCoBackground)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 8)
+                    )
+                }
+            }
+        }
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    HeaderView(heading: "북마크 관리",isBookmarkView: true)
+    SubheaderView(subheading: "북마크하신 코인들을 분석해봤어요")
+}
