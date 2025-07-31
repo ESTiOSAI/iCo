e5451192e6a47bf9708c1932a73b0fd68f4f9706
kanghun1121 <kanghun1121@naver.com>
2025-07-31 15:18:42 +0900

    refactor: MarketDTO 변수명 변경 market -> coinID

--- AIProject/AIProject/Data/Response/UpbitResponse/MarketDTO.swift
+++ AIProject/AIProject/Data/Response/UpbitResponse/MarketDTO.swift
@@ -10,14 +10,14 @@ import Foundation
 /// 업비트에서 거래 가능한 종목 목록 DTO
 struct MarketDTO: Codable {
     /// 마켓 식별자
-    let market: String
+    let coinID: String
     /// 코인의 한글 이름 ex) 비트코인
     let koreanName: String
     /// 코인의 영문 이름 ex) Bitcoin
     let englishName: String
 
     enum CodingKeys: String, CodingKey {
-        case market
+        case coinID = "market"
         case koreanName = "korean_name"
         case englishName = "english_name"
     }
