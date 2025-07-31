d0dee925e933b4fdc2595d31637569f037e328df
kangho Lee <ibcylon@naver.com>
2025-07-31 14:36:47 +0900

    feat: coin list view

--- AIProject/AIProject/Features/Market/CoinListView.swift
+++ AIProject/AIProject/Features/Market/CoinListView.swift
@@ -7,37 +7,72 @@
 
 import SwiftUI
 
-
 struct CoinListView: View {
+    @State var coins: [CoinListModel]
+    
     var body: some View {
         List {
-            ForEach(1...10, id: \.self) { _ in
+            HStack(spacing: 60) {
                 HStack {
-                    HStack {
-                        Image(systemName: "swift")
-                        
-                        Text("비트코인")
-                            .bold()
-                        
-                        Text("BTC")
-                            .font(.callout)
-                            .foregroundStyle(.secondary)
-                    }
-                    
-                    Text(1333, format: .number)
-                        .foregroundStyle(.red)
-                    Text(3, format: .percent.precision(.fractionLength(2)))
-                        .foregroundStyle(.red)
-                    
-                    Text(162140000, format: .currency(code: "KRW"))
-                        .fontWeight(.medium)
-                        .font(.system(size: 12))
+                    Text("한글명")
+                    Image(systemName: "arrow.up.arrow.down")
                 }
+                
+                HStack {
+                    Text("현재가")
+                    
+                    Text("전일대비")
+                }
+                
+                Text("거래대금")
+            }
+            .fontWeight(.regular)
+            .font(.system(size: 11))
+            .foregroundStyle(.aiCoLabel)
+            
+            ForEach(coins) { coin in
+                NavigationLink {
+                    MockDetailView(coin: coin)
+                } label: {
+                    HStack {
+                        HStack {
+                            Image(systemName: "swift")
+                            
+                            VStack(alignment: .leading) {
+                                Text(coin.name)
+                                    .font(.system(size: 14))
+                                
+                                Text(coin.coinName)
+                                    .font(.system(size: 12))
+                                    .font(.callout)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .background(.red)
+                            .frame(minWidth: 80, maxWidth: 100)
+                        }
+                        
+                        Text(coin.currentPrice, format: .number)
+                            .font(.system(size: 14))
+                            .foregroundStyle(.red)
+                        
+                        Text(coin.changePrice, format: .percent.precision(.fractionLength(2)))
+                            .font(.system(size: 14))
+                            .foregroundStyle(.red)
+                        
+                        Text(162140000, format: .number)
+                            .font(.system(size: 12))
+                        Text("원")
+                            .font(.system(size: 12))
+                    }
+                    .fontWeight(.medium)
+                    .foregroundStyle(.aiCoLabel)                    
+                }
+
             }
         }
     }
 }
 
 #Preview {
-    CoinListView()
+    CoinListView(coins: CoinListModel.preview)
 }
