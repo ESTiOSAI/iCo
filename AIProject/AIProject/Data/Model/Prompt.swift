76375ef9127198ddd994a63836bef4d1758240e4
kanghun1121 <kanghun1121@naver.com>
2025-08-01 17:08:45 +0900

    feat: 모델 추가

---
+++ AIProject/AIProject/Data/Model/Prompt.swift
@@ -0,0 +1,32 @@
+//
+//  Prompt.swift
+//  AIProject
+//
+//  Created by 강대훈 on 8/1/25.
+//
+
+/// 정형화된 프롬프트 객체
+enum Prompt {
+    case recommendCoin(preference: String, bookmark: String)
+
+    var content: String {
+        switch self {
+        case .recommendCoin(let preference, let bookmark):
+"""
+                     코인 5개를 추천해줘.
+            
+                     현재 나의 코인 투자 선호도는 다음과 같아.
+                     -> \(preference)
+            
+                     현재 내가 북마크한 코인은 다음과 같아.
+                     -> \(bookmark)
+            
+                     JSON 형식으로 요청할거고, JSON 형식으로만 응답을 줘
+                     struct RecommendCoin: Codable {
+                         let name: String
+                         let symbol: String
+                     }
+"""
+        }
+    }
+}
---
+++ AIProject/AIProject/Data/Model/RecommendCoin.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendCoin.swift
+//  AIProject
+//
+//  Created by 강대훈 on 8/1/25.
+//
+
+struct RecommendCoin: Identifiable {
+    var id: String { coinID }
+
+    let comment: String
+    let coinID: String
+}
