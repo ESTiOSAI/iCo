fe0b22de5e68a745b3943cf9b713fb6e53eeb20e
kanghun1121 <kanghun1121@naver.com>
2025-08-04 14:36:01 +0900

    feat: 코인 추천 로직 구현

--- AIProject/AIProject/Features/Dashboard/ViewModel/DashboardViewModel.swift
+++ AIProject/AIProject/Features/Dashboard/ViewModel/DashboardViewModel.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 final class DashboardViewModel: ObservableObject {
     /// 추천 코인 배열
-    @Published var recommendCoin: [RecommendCoin] = []
+    @Published var recommendCoins: [RecommendCoin] = []
 
     private var alanService: AlanAPIService
     private var upbitService: UpBitAPIService
@@ -18,14 +18,50 @@ final class DashboardViewModel: ObservableObject {
         alanService = AlanAPIService()
         upbitService = UpBitAPIService()
     }
+    
+    /// 비동기로 추천 코인 목록을 가져와 `recommendCoins`에 할당합니다.
+    func getRecommendCoin() async throws {
+        let prompt = Prompt.recommendCoin(preference: "초보자", bookmark: "비트코인, 리트코인")
+        let jsonString = try await alanService.fetchAnswer(content: prompt.content).content
 
-    func getRecommendCoin() {
-        recommendCoin = [
-            RecommendCoin(comment: "좋다!", coinID: "KRW-BTCa"),
-            RecommendCoin(comment: "좋다!", coinID: "KRW-BTCb"),
-            RecommendCoin(comment: "좋다!", coinID: "KRW-BTCc"),
-            RecommendCoin(comment: "좋다!", coinID: "KRW-BTCd"),
-            RecommendCoin(comment: "좋다!", coinID: "KRW-BTCe"),
-        ]
+        if let jsonData = jsonString.data(using: .utf8) {
+            let recommendCoinDTOs = try JSONDecoder().decode([RecommendCoinDTO].self, from: jsonData)
+            let results = try await fetchRecommendCoins(from: recommendCoinDTOs)
+
+            Task { @MainActor in
+                recommendCoins = results
+            }
+        }
+    }
+
+    private func fetchRecommendCoins(from dtos: [RecommendCoinDTO]) async throws -> [RecommendCoin] {
+        try await withThrowingTaskGroup(of: RecommendCoin?.self) { group in
+            for dto in dtos {
+                group.addTask {
+                    guard let data = try await self.upbitService.fetchQuotes(id: dto.symbol).first else {
+                        return nil
+                    }
+
+                    return RecommendCoin(
+                        coinImage: nil,
+                        comment: dto.comment,
+                        coinID: data.coinID,
+                        name: dto.name,
+                        tradePrice: data.tradePrice,
+                        changeRate: data.changeRate
+                    )
+                }
+            }
+
+            var results: [RecommendCoin] = []
+
+            for try await coin in group {
+                if let coin = coin {
+                    results.append(coin)
+                }
+            }
+
+            return results
+        }
     }
 }
