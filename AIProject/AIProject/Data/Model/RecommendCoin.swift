64c46ffffeb608107557293b265266391ffc9f66
kanghun1121 <kanghun1121@naver.com>
2025-08-04 14:36:31 +0900

    refactor: 코인 추천 모델 프로퍼티 추가

--- AIProject/AIProject/Data/Model/RecommendCoin.swift
+++ AIProject/AIProject/Data/Model/RecommendCoin.swift
@@ -5,9 +5,15 @@
 //  Created by 강대훈 on 8/1/25.
 //
 
+import SwiftUI
+
 struct RecommendCoin: Identifiable {
     var id: String { coinID }
 
+    let coinImage: UIImage?
     let comment: String
     let coinID: String
+    let name: String
+    let tradePrice: Double
+    let changeRate: Double
 }
