bb39b5697a64d2bb5c4a4550bfe6cfd0fbcb3d17
kangho Lee <ibcylon@naver.com>
2025-08-04 18:15:11 +0900

    feat: coin List UI 수정 및 viewModel 분리

--- AIProject/AIProject/Features/Market/CoinListView.swift
+++ AIProject/AIProject/Features/Market/CoinListView.swift
@@ -7,56 +7,6 @@
 
 import SwiftUI
 
-@Observable
-class CoinListViewModel {
-    private let socket: WebSocketClient
-    private let upbitService: UpBitAPIService
-    
-    var coins: [CoinListModel] = []
-    
-    init(socket: WebSocketClient) {
-        self.socket = socket
-        self.upbitService = UpBitAPIService()
-    }
-    
-    func connect() async {
-        try? await socket.connect()
-    }
-    
-    func disconnect() {
-        socket.disconnect()
-    }
-    
-    func fetchTickerbyMarket() async -> [String] {
-        []
-    }
-    
-    func fetchInitial() async {
-        self.coins = await fetchMarketCoinData()
-    }
-    
-    private func fetchMarketCoinData() async -> [CoinListModel] {
-        async let coins = (try? await upbitService.fetchMarkets()) ?? []
-        async let tickers = (try? await upbitService.fetchTicker(by: "KRW")) ?? []
-        
-        let result = await coins.reduce(into: [String: (korean: String, english: String)]()){ acc, coins in
-                    
-            acc[coins.coinID] = (coins.koreanName, coins.englishName)
-        }
-        
-        return await tickers.compactMap { ticker in
-            CoinListModel(
-                coinID: ticker.coinID,
-                image: "",
-                name: result[ticker.coinID]?.korean ?? "없음",
-                currentPrice: ticker.tradePrice,
-                changePrice: ticker.changeRate,
-                tradeAmount: ticker.accTradePrice
-            )
-        }
-    }
-}
-
 struct CoinListView: View {
     
     let viewModel = CoinListViewModel(socket: .init())
@@ -65,58 +15,13 @@ struct CoinListView: View {
         VStack {
             
             List {
-                HStack(spacing: 60) {
-                    HStack {
-                        Text("한글명")
-                        Image(systemName: "arrow.up.arrow.down")
-                    }
-                    
-                    HStack {
-                        Text("현재가")
-                        
-                        Text("전일대비")
-                    }
-                    
-                    Text("거래대금")
-                }
-                .fontWeight(.regular)
-                .font(.system(size: 11))
-                .foregroundStyle(.aiCoLabel)
+                CoinListHeaderView()
+                    .fontWeight(.regular)
+                    .font(.system(size: 11))
+                    .foregroundStyle(.aiCoLabel)
                 
                 ForEach(viewModel.coins) { coin in
-//                    NavigationLink {
-//                        CoinDetailView(coin: Coin(id: coin.coinID, koreanName: coin.name))
-//                    } label: {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(coin.name)
-                                        .font(.system(size: 14))
-                                    
-                                    Text(coin.coinName)
-                                        .font(.system(size: 12))
-                                        .font(.callout)
-                                        .foregroundStyle(.secondary)
-                                }
-                                
-                                Text(coin.currentPrice, format: .number)
-                                    .font(.system(size: 14))
-                                    .foregroundStyle(.red)
-                                
-                                Text(coin.changePrice, format: .percent.precision(.fractionLength(2)))
-                                    .font(.system(size: 14))
-                                    .foregroundStyle(.red)
-                                
-                                Text(162140000, format: .number)
-                                    .font(.system(size: 12))
-                                Text("원")
-                                    .font(.system(size: 12))
-                            }
-                            .fontWeight(.medium)
-                            .foregroundStyle(.aiCoLabel)
-                            .frame(maxWidth: .infinity)
-//                        }
-                    }
+                    CoinCell(coin: coin)
                 }
             }
             .task {
@@ -126,6 +31,69 @@ struct CoinListView: View {
             .onDisappear {
                 viewModel.disconnect()
             }
+        }
+    }
+}
+
+fileprivate struct CoinListHeaderView: View {
+    var body: some View {
+        HStack(spacing: 60) {
+            HStack {
+                Text("한글명")
+                Image(systemName: "arrow.up.arrow.down")
+            }
+            
+            HStack {
+                Text("현재가")
+                    .frame(maxWidth: 80, alignment: .trailing)
+                
+                Text("전일대비")
+                    .frame(maxWidth: 40, alignment: .trailing)
+            }
+            
+            Text("거래대금")
+        }
+    }
+}
+
+fileprivate struct CoinCell: View {
+    let coin: CoinListModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: 8) {
+                VStack(alignment: .leading) {
+                    Text(coin.name)
+                        .font(.system(size: 14))
+                    
+                    Text(coin.coinName)
+                        .font(.system(size: 12))
+                        .font(.callout)
+                        .foregroundStyle(.secondary)
+                }
+                .frame(maxWidth: 100, alignment: .leading)
+                
+                Text(coin.currentPrice, format: .number)
+                    .font(.system(size: 12))
+                    .foregroundStyle(.red)
+                    .frame(maxWidth: 75, alignment: .trailing)
+                
+                Text(coin.changePrice, format: .percent.precision(.fractionLength(2)))
+                    .font(.system(size: 12))
+                    .foregroundStyle(.red)
+                    .frame(maxWidth: 40, alignment: .trailing)
+                
+                HStack(spacing: 0) {
+                    Text(coin.tradeAmount, format: .number
+                        .font(.system(size: 12))
+                    Text("원")
+                        .font(.system(size: 12))
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .fontWeight(.medium)
+            .foregroundStyle(.aiCoLabel)
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
     }
 }
---
+++ AIProject/AIProject/Features/Market/CoinListViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  CoinListViewModel.swift
+//  AIProject
+//
+//  Created by kangho lee on 8/4/25.
+//
+
+import Foundation
+
+@Observable
+class CoinListViewModel {
+    private let socket: WebSocketClient
+    private let upbitService: UpBitAPIService
+    
+    var coins: [CoinListModel] = []
+    
+    init(socket: WebSocketClient) {
+        self.socket = socket
+        self.upbitService = UpBitAPIService()
+    }
+    
+    func connect() async {
+        try? await socket.connect()
+    }
+    
+    func disconnect() {
+        socket.disconnect()
+    }
+    
+    func fetchInitial() async {
+        self.coins = await fetchMarketCoinData()
+    }
+    
+    private func fetchMarketCoinData() async -> [CoinListModel] {
+        async let coins = (try? await upbitService.fetchMarkets()) ?? []
+        async let tickers = (try? await upbitService.fetchTicker(by: "KRW")) ?? []
+        
+        let result = await coins.reduce(into: [String: (korean: String, english: String)]()){ acc, coins in
+                    
+            acc[coins.coinID] = (coins.koreanName, coins.englishName)
+        }
+        
+        return await tickers.compactMap { ticker in
+            CoinListModel(
+                coinID: ticker.coinID,
+                image: "",
+                name: result[ticker.coinID]?.korean ?? "없음",
+                currentPrice: ticker.tradePrice,
+                changePrice: ticker.changeRate,
+                tradeAmount: ticker.accTradePrice
+            )
+        }
+    }
+}
