6dfbcf36a34f4570dd14bd2a6a9e7e1e3c22a2a6
kanghun1121 <kanghun1121@naver.com>
2025-08-05 23:37:51 +0900

    feat: 검색 뷰모델 생성 및 로직 구현

---
+++ AIProject/AIProject/Features/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  SearchViewModel.swift
+//  AIProject
+//
+//  Created by 강대훈 on 8/5/25.
+//
+
+import SwiftUI
+import AsyncAlgorithms
+
+final class SearchViewModel: ObservableObject {
+    @Published var bookMarkCoins: [Coin] = []
+    @Published var relatedCoins: [Coin] = []
+
+    private var upbitService: UpBitAPIService
+
+    private var coins: [Coin] = []
+
+    private var continuation: AsyncStream<String>.Continuation?
+    private var task: Task<Void, Never>?
+
+    init() {
+        upbitService = UpBitAPIService()
+        setupCoinData()
+        observeStream()
+    }
+    
+    /// 북마크에 저장된 코인을 받아옵니다.
+    func loadBookMarkCoins() {
+        bookMarkCoins = [
+            Coin(id: "KRW-BTC", koreanName: "비트코인"),
+            Coin(id: "KRW-ETC", koreanName: "이더리움"),
+            Coin(id: "KRW-LTC", koreanName: "리트코인"),
+        ]
+    }
+    
+    /// 검색 키워드를 스트림에 전달합니다.
+    /// - Parameter keyword: 사용자가 입력한 검색어 문자열입니다.
+    func sendKeyword(with keyword: String) async {
+        continuation?.yield(keyword)
+    }
+
+    /// 업비트에 존재하는 모든 코인 목록을 받아옵니다.
+    private func setupCoinData() {
+        Task {
+            do {
+                let coinDTOs = try await upbitService.fetchMarkets()
+                coins = coinDTOs.map { Coin(id: $0.coinID, koreanName: $0.koreanName) }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    /// 사용자 검색어 스트림을 관찰하고 검색을 트리거합니다.
+    ///
+    /// - 해당 메소드는 0.3초의 디바운스가 적용되어 있습니다.
+    private func observeStream() {
+        let stream = AsyncStream<String> { continuation in
+            self.continuation = continuation
+        }
+
+        task = Task {
+            for await keyword in stream.removeDuplicates().debounce(for: .seconds(0.3)) {
+                await performSearch(with: keyword)
+            }
+        }
+    }
+
+    /// 주어진 키워드를 기반으로 관련 코인 목록을 필터링하여 갱신합니다.
+    ///
+    /// - Parameter keyword: 사용자가 입력한 검색어입니다.
+    @MainActor
+    private func performSearch(with keyword: String) {
+        guard !keyword.isEmpty else { return }
+
+        relatedCoins = coins.filter { $0.koreanName.contains(keyword) }
+    }
+
+    deinit {
+        task?.cancel()
+        task = nil
+    }
+}
