323efa6f0fd91043b4c124afb52641a06290ec23
백현진 <guswlsrlacl@naver.com>
2025-08-07 15:34:12 +0900

    fix: 주석 제거 및 coinKoreaName로 변경

--- AIProject/AIProject/Features/MyPage/View/CoinRowView.swift
+++ AIProject/AIProject/Features/MyPage/View/CoinRowView.swift
@@ -7,64 +7,6 @@
 
 import SwiftUI
 
-//struct CoinRowView: View {
-//    let coin: CoinListModel
-//
-//    var body: some View {
-//        HStack(alignment: .top, spacing: 0) {
-//            HStack(spacing: 8) {
-//                Image(systemName: "bitcoinsign.circle.fill")
-//                    .resizable()
-//                    .frame(width: 30, height: 30)
-//                    .background(Circle().fill(.yellow))
-//                    .clipShape(Circle())
-//
-//                VStack(alignment: .leading, spacing: 2) {
-//                    Text(coin.name)
-//                        .font(.system(size: 15))
-//                        .foregroundColor(.aiCoLabel)
-//
-//                    Text(coin.coinID)
-//                        .font(.caption)
-//                        .foregroundColor(.aiCoLabelSecondary)
-//                }
-//
-//            }
-//            .frame(maxWidth: .infinity, alignment: .leading)
-//
-//            // 가격
-//            VStack(alignment: .trailing, spacing: 2) {
-//                HStack(spacing: 2) {
-//                    Text(coin.currentPrice, format: .number)
-//                    Text("원")
-//                }
-//                .foregroundColor(Color.aiCoLabel)
-//                .font(.system(size: 12))
-//
-//                HStack(spacing: 2) {
-//                    Text(coin.changePrice, format: .number)
-//                    Text("%")
-//                }
-//                .font(.caption)
-//                .foregroundColor(Color.aiCoPositive)
-//            }
-//            .frame(width: 100, alignment: .trailing)
-//
-//            // 거래 대금
-//            VStack(alignment: .trailing, spacing: 2) {
-//                HStack(spacing: 2) {
-//                    Text(coin.tradeAmount.formatMillion)
-//                }
-//                .font(.system(size: 15))
-//                .foregroundColor(.aiCoLabel)
-//            }
-//            .frame(width: 100, alignment: .trailing)
-//        }
-//        .padding(.vertical, 8)
-//        .padding(.horizontal)
-//    }
-//}
-
 struct CoinRowView: View {
     let coin: BookmarkEntity
 
@@ -83,7 +25,7 @@ struct CoinRowView: View {
                 )
 
             VStack(alignment: .leading, spacing: 2) {
-                Text(coin.coinID)
+                Text(coin.coinKoreanName)
                     .font(.system(size: 15, weight: .semibold))
                     .foregroundColor(.primary)
 
