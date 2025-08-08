946656b077dee29c7573366b6ae40f125646f9d2
Minji Kang <aldalddl2007@gmail.com>
2025-08-08 14:24:45 +0900

    feat: 테마 변경 UI 구현 (새 디자인 적용 전)

--- AIProject/AIProject/Features/MyPage/View/ThemeView.swift
+++ AIProject/AIProject/Features/MyPage/View/ThemeView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+enum Theme: String, CaseIterable {
+    case basic
+    case pop
+    case classic
+}
+
 struct ThemeView: View {
+    @State private var selectedTheme: Theme = .basic
+    
     var body: some View {
         VStack {
-            HeaderView(heading: "차트 색상 변경")
+            HeaderView(heading: "테마 변경")
+                .padding(.top, 20)
+                .padding(.bottom, 20)
+            SubheaderView(subheading: "차트 색상 변경")
+                .padding(.bottom, 20)
+            
+            ThemeRow(
+                title: "기본",
+                positiveColor: .aiCoPositive,
+                negativecColor: .aiCoNegative,
+                isSelected: selectedTheme == .basic
+            ) {
+                selectedTheme = .basic
+            }
+
+            ThemeRow(
+                title: "팝",
+                positiveColor: .red,
+                negativecColor: Color(UIColor.systemMint),
+                isSelected: selectedTheme == .pop
+            ) {
+                selectedTheme = .pop
+            }
+            
+            ThemeRow(
+                title: "고전",
+                positiveColor: Color(UIColor.systemPink),
+                negativecColor: .green,
+                isSelected: selectedTheme == .classic
+            ) {
+                selectedTheme = .classic
+            }
+            
             Spacer()
         }
     }
@@ -18,4 +58,42 @@ struct ThemeView: View {
 
 #Preview {
     ThemeView()
+}
+
+struct ThemeRow: View {
+    let title: String
+    let positiveColor: Color
+    let negativecColor: Color
+    let isSelected: Bool
+    let onTap: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.body)
+            
+            Spacer()
+            
+            HStack(spacing: 0) {
+                positiveColor
+                negativecColor
+            }
+            .frame(width: 40, height: 20)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+        }
+        .padding(20)
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(isSelected ? Color.blue.opacity(0.1) : .aiCoBackground)
+        }
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isSelected ? Color.blue.opacity(0.1) : .aiCoBackground, lineWidth: 1)
+        }
+        .onTapGesture {
+            onTap()
+        }
+        .padding(.horizontal, 16)
+        .padding(.bottom, 16)
+    }
 }
