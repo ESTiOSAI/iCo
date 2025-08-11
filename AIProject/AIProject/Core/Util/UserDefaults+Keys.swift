03939c188e32010630261353f3cd9ba8bdab1704
jihyeonjjang <jihyeon9975@gmail.com>
2025-08-11 10:18:06 +0900

    feat: 온보딩 - 투자 성향 선택 구현

---
+++ AIProject/AIProject/Core/Util/UserDefaults+Keys.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaults+Keys.swift
+//  AIProject
+//
+//  Created by 장지현 on 8/11/25.
+//
+
+import Foundation
+
+extension UserDefaults {
+    enum Keys {
+        static let investmentType = "investmentType"
+    }
+}
--- AIProject/AIProject/Features/Onboarding/LastOnboardingPage.swift
+++ AIProject/AIProject/Features/Onboarding/LastOnboardingPage.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct LastOnboardingPage: View {
+    @State private var selectedType: RiskTolerance?
+    @State var isSelected = false
     var onFinish: () -> Void
     
     var body: some View {
         VStack {
-            Text("시작해볼까요?")
-                .font(.title)
+            Spacer()
             
-            Button("시작하기") {
+            HeaderView(heading: "나의 투자 성향 고르기")
+                .padding(.bottom, 16)
+            
+            SubheaderView(subheading: "아래 5가지 중 본인에게 맞는\n투자 성향을 선택해주세요.", description: "투자 성향은 추후 추천 콘텐츠와 서비스에 반영됩니다.")
+                .padding(.bottom, 16)
+            
+            ForEach(RiskTolerance.allCases, id: \.self) { type in
+                RoundedRectangleFillButton(
+                    title: type.rawValue,
+                    isSelected: Binding(get: { selectedType == type }, set: { _ in })) {
+                        selectedType = type
+                        isSelected = true
+                    }
+                    .animation(.easeInOut(duration: 0.15), value: selectedType)
+            }
+            .padding(.horizontal, 16)
+            
+            Spacer()
+            
+            RoundedRectangleFillButton(title: "시작하기", isSelected: $isSelected) {
+                if let selected = selectedType {
+                    UserDefaults.standard.set(selected.rawValue, forKey: UserDefaults.Keys.investmentType)
+                    
+                    let savedValue = UserDefaults.standard.string(forKey: UserDefaults.Keys.investmentType)
+                    print("저장된 투자 성향: \(savedValue ?? "없음")")
+                }
                 onFinish()
             }
-            .padding()
+            .disabled(selectedType == nil)
+            .opacity(selectedType == nil ? 0.5 : 1)
+            .padding(.horizontal, 16)
+            
+            Spacer()
         }
     }
 }
