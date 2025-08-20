385e9eb6f4c7d173498895a58161eba8a889b741
kangho Lee <ibcylon@naver.com>
2025-08-20 22:05:24 +0900

    refactor: searchbar 애니메이션 적용

--- AIProject/AIProject/Features/Search/SearchBarView.swift
+++ AIProject/AIProject/Features/Search/SearchBarView.swift
@@ -16,10 +16,10 @@ struct SearchBarView: View {
             HStack {
                 Image(systemName: "magnifyingglass")
 
-
                 TextField("코인 이름으로 검색하세요", text: $searchText)
                     .padding(.horizontal, 8)
                     .focused($isFocused)
+                    .submitLabel(.search)
 
                 if !searchText.isEmpty {
                     CircleDeleteButton(fontSize: 9) {
@@ -29,22 +29,28 @@ struct SearchBarView: View {
             }
             .padding(.horizontal, 12)
             .padding(.vertical, 14)
+            .shadow(radius: isFocused ? 1 : 0)
             .background {
                 RoundedRectangle(cornerRadius: 15)
                     .fill(.aiCoBackground)
             }
+            .animation(.snappy(duration: 0.2), value: isFocused)
 
             if isFocused {
                 Button {
 					// TODO: 취소 시에 검색 View 내리는 동작
-                    isFocused = false
+                    withAnimation(.snappy) {
+                        isFocused = false
+                        searchText = ""
+                    }
                 } label: {
                     Text("취소")
                         .foregroundStyle(.aiCoNegative)
                         .font(.system(size: 13))
+                        .padding(8)
                 }
+                .transition(.move(edge: .trailing).combined(with: .opacity))
             }
         }
-        .padding(.horizontal, 16)
     }
 }
