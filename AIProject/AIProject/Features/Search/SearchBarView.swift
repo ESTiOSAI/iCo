de043e6038e19b4e756ddc37d1535eb1cc91fe6b
kanghun1121 <kanghun1121@naver.com>
2025-08-20 13:49:40 +0900

    feat: 취소 버튼 추가

--- AIProject/AIProject/Features/Search/SearchBarView.swift
+++ AIProject/AIProject/Features/Search/SearchBarView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct SearchBarView: View {
     @Binding var searchText: String
+    @FocusState private var isFocused: Bool
 
     var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
+        HStack(spacing: 10) {
+            HStack {
+                Image(systemName: "magnifyingglass")
 
-            TextField("코인 이름으로 검색하세요", text: $searchText)
-                .padding(.horizontal, 8)
 
-            if !searchText.isEmpty {
-                CircleDeleteButton(fontSize: 9) {
-                    searchText = ""
+                TextField("코인 이름으로 검색하세요", text: $searchText)
+                    .padding(.horizontal, 8)
+                    .focused($isFocused)
+
+                if !searchText.isEmpty {
+                    CircleDeleteButton(fontSize: 9) {
+                        searchText = ""
+                    }
                 }
             }
-        }
-        .padding(.horizontal, 12)
-        .padding(.vertical, 14)
-        .background {
-            RoundedRectangle(cornerRadius: 8)
-                .fill(.aiCoBackground)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 14)
+            .background {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(.aiCoBackground)
+            }
+
+            if isFocused {
+                Button {
+					// TODO: 취소 시에 검색 View 내리는 동작
+                    isFocused = false
+                } label: {
+                    Text("취소")
+                        .foregroundStyle(.aiCoNegative)
+                        .font(.system(size: 13))
+                }
+            }
         }
         .padding(.horizontal, 16)
     }
