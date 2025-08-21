51f8ccde7f7f4dc7c80fba0acfd56ed3c46095fb
kitcat-s <kitcat92.seo@gmail.com>
2025-08-22 07:03:34 +0900

    feat: iPad일 때는 RecomendationPlaceholderCardView 숨기기

--- AIProject/AIProject/Features/Dashboard/View/RecomendationPlaceholderCardView.swift
+++ AIProject/AIProject/Features/Dashboard/View/RecomendationPlaceholderCardView.swift
@@ -10,38 +10,58 @@ import SwiftUI
 /// DefaultProgressView가 공중에 어색하게 떠있는 것을 개선하기 위해 만든 카드의 플레이스홀더뷰
 /// 사실 플레이스홀더 뷰를 메인으로 띄우고, 네트워크 로드가 완료되면 내부 컨텐츠만 바꾸는 방식으로 가고 싶은데... 추후에 변경하는 것으로...
 struct RecomendationPlaceholderCardView: View {
+    @Environment(\.horizontalSizeClass) var hSizeClass
+    
     var status: DefaultProgressView.Status
     var message: String
     var action: () -> Void
     
     var body: some View {
         GeometryReader { geoProxy in
-            HStack(alignment: .bottom, spacing: 16) {
-                Group {
-                    Color.aiCoBackgroundWhite.opacity(0.9)
-                        .background(.ultraThinMaterial)
-                        .frame(width: 100, height: .cardHeight * .cardHeightMultiplier)
-                    
-                    DefaultProgressView(status: status, message: message) {
-                        action()
-                    }
-                    .frame(width: geoProxy.size.width * 0.82, height: .cardHeight)
-                    .background(
-                        ZStack {
-                            Rectangle().fill(.ultraThinMaterial)
+            ZStack {
+                if hSizeClass == .compact {
+                    HStack(alignment: .bottom, spacing: 16) {
+                        Group {
                             Color.aiCoBackgroundWhite.opacity(0.9)
+                                .background(.ultraThinMaterial)
+                                .frame(width: 100, height: .cardHeight * .cardHeightMultiplier)
+                            
+                            Color.aiCoBackgroundWhite.opacity(0.9)
+                                .background(.ultraThinMaterial)
+                                .frame(
+                                    width: geoProxy.size.width - (20 * 2) - (16 * 2) - 8,
+                                    height: .cardHeight
+                                )
+                            
+                            Color.aiCoBackgroundWhite.opacity(0.9)
+                                .background(.ultraThinMaterial)
+                                .frame(width: 100, height: .cardHeight * .cardHeightMultiplier)
                         }
-                    )
-                    
-                    Color.aiCoBackgroundWhite.opacity(0.9)
-                        .background(.ultraThinMaterial)
-                        .frame(width: 100, height: .cardHeight * .cardHeightMultiplier)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 24)
+                                .stroke(.defaultGradient, lineWidth: 0.5)
+                        }
+                    }
+                    .frame(maxWidth: geoProxy.size.width - (40 * 2))
                 }
-                .clipShape(RoundedRectangle(cornerRadius: 24))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 24)
-                        .stroke(.defaultGradient, lineWidth: 0.5)
+                
+                VStack {
+                    DefaultProgressView(status: status, message: message) { action() }
+                        .offset(y: -8)
                 }
+                .background(hSizeClass == .regular ?
+                    ZStack {
+                        Rectangle().fill(.ultraThinMaterial)
+                        Color.aiCoBackgroundWhite.opacity(0.9)
+                    }
+                            : nil
+                )
+                .frame(maxWidth: hSizeClass == .regular ? 400 : .infinity)
+                .frame(height: .cardHeight)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay(
+                    hSizeClass == .regular ? RoundedRectangle(cornerRadius: 20).stroke(.defaultGradient, lineWidth: 0.5) : nil)
             }
             .frame(width: geoProxy.size.width)
         }
