3d7475c63df7e684be8ababc582c224b1847a2a2
kitcat-s <kitcat92.seo@gmail.com>
2025-08-24 23:01:56 +0900

    feat: Formatter + 계산 속성 추가하기

---
+++ AIProject/AIProject/Core/Util/Date+Util.swift
@@ -0,0 +1,32 @@
+//
+//  Date+Util.swift
+//  AIProject
+//
+//  Created by Kitcat Seo on 8/24/25.
+//
+
+import Foundation
+
+/// `Date` 타입에 날짜와 시간을 지정된 형식으로 포맷팅하는 기능을 추가하는 확장.
+///
+/// - Static Property:
+///   - dateAndTimeFormatter: `"yyyy.MM.dd HH:mm"` 형식의 `DateFormatter`.
+///
+/// - Computed Property:
+///   - dateAndTime: `Date` 인스턴스를 지정된 형식의 문자열로 변환.
+///
+/// 사용 예시:
+/// let now = Date()
+/// print(now.dateAndTime) // "2025.08.24 22:15"
+extension Date {
+    static let dateAndTimeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd HH:mm"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter
+    }()
+    
+    var dateAndTime: String {
+        Date.dateAndTimeFormatter.string(from: self)
+    }
+}
