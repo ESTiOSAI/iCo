05f0ca94470cdb80c4b45c27a22040b52c892d46
kangho Lee <ibcylon@naver.com>
2025-08-26 06:20:30 +0900

    refactor: subview 분리

--- AIProject/AIProject/Features/Market/MarketView.swift
+++ AIProject/AIProject/Features/Market/MarketView.swift
@@ -89,44 +89,6 @@ struct MarketView: View {
     }
 }
 
-fileprivate struct RecentCoinSectionView: View {
-    let coins: [Coin]
-    let deleteAction: (Coin) -> Void
-    let tapAction: (CoinID) -> Void
-
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(coins) { coin in
-                    HStack(spacing: 8) {
-                        Text(coin.koreanName)
-                            .font(.system(size: 14))
-
-                        Button {
-                            deleteAction(coin)
-                        } label: {
-                            Image(systemName: "xmark")
-                                .resizable()
-                                .frame(width: 10, height: 10)
-                                .foregroundStyle(.aiCoLabelSecondary)
-                        }
-                    }
-                    .padding(.vertical, 8)
-                    .padding(.horizontal, 12)
-                    .background {
-                        Capsule().stroke(.defaultGradient, lineWidth: 0.5)
-                    }
-                    .onTapGesture {
-                        tapAction(coin.id)
-                    }
-                }
-            }
-        }
-        .safeAreaPadding(.horizontal, 16)
-        .scrollIndicators(.hidden)
-    }
-}
-
 #Preview {
     MarketView(
         coinService: UpBitAPIService(),
