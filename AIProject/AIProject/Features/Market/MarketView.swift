5d7b4f9df41451d8b74aefc77e3bddfaa813f396
kangho Lee <ibcylon@naver.com>
2025-08-27 19:31:12 +0900

    refactor: tap gesture 영역 개선

--- AIProject/AIProject/Features/Market/MarketView.swift
+++ AIProject/AIProject/Features/Market/MarketView.swift
@@ -8,82 +8,80 @@
 import SwiftUI
 
 struct MarketView: View {
-
+    
     @State var store: MarketStore
-
+    
     @State private var searchText: String = ""
     @State private var selectedCoinID: CoinID?
     @State private var columnVisibility: NavigationSplitViewVisibility = .all
-
+    
     @FetchRequest(
         fetchRequest: SearchRecordEntity.recent(),
         animation: .default
     )
     private var records: FetchedResults<SearchRecordEntity>
-
+    
     init(coinService: UpBitAPIService, tickerService: RealTimeTickerProvider) {
         store = MarketStore(coinService: coinService, tickerService: tickerService)
     }
-
+    
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility, preferredCompactColumn: .constant(.sidebar)) {
-            VStack(spacing: 0) {
-                HeaderView(heading: "마켓")
-                    .frame(maxWidth: .infinity)
-                    .contentShape(.rect)
-                    .onTapGesture {
-                        UIApplication.shared.endEditing()
+            ZStack {
+                Color.clear
+                    .dissmissKeyboardOnTap()
+                
+                VStack(spacing: 0) {
+                    HeaderView(heading: "마켓")
+                    
+                    SearchBarView(searchText: $searchText)
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 20)
+                    
+                    if !records.isEmpty {
+                        RecentCoinSectionView(coins: records.compactMap { store.coinMeta[$0.query] }, deleteAction: { coin in
+                            store.deleteRecord(coin.id)
+                        }) { selectedCoinID = $0 }
+                            .padding(.bottom, 16)
                     }
-                
-                SearchBarView(searchText: $searchText)
-                    .padding(.horizontal, 16)
-                    .padding(.bottom, 20)
-                
-                
-                if !records.isEmpty {
-                    RecentCoinSectionView(coins: records.compactMap { store.coinMeta[$0.query] }, deleteAction: { coin in
-                        store.deleteRecord(coin.id)
-                    }) { selectedCoinID = $0 }
-                        .padding(.bottom, 16)
-                }
-
-                VStack(spacing: 16) {
+                    
                     HStack(spacing: 16) {
                         RoundedRectangleButton(title: "전체", isActive: store.filter == .none) {
                             store.filter = .none
                         }
-
+                        
                         RoundedRectangleButton(title: "북마크", isActive: store.filter == .bookmark) {
                             store.filter = .bookmark
                         }
+                        
+                        Spacer()
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, 4)
-                    .contentShape(.rect)
-                    .onTapGesture {
-                        UIApplication.shared.endEditing()
+                    .frame(alignment: .leading)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 20)
+                    
+                    VStack(spacing: 16) {
+                        CoinListView(store: store, selectedCoinID: $selectedCoinID, searchText: $searchText)
                     }
-
-                    CoinListView(store: store, selectedCoinID: $selectedCoinID, searchText: $searchText)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    .refreshable {
+                        Task {
+                            await store.refresh()
+                        }
+                    }
+                    .onChange(of: searchText, { oldValue, newValue in
+                        Task {
+                            await store.search(newValue)
+                        }
+                    })
+                    .task {
+                        await store.load()
+                    }
                 }
-                .padding(.horizontal, 16)
-                .padding(.bottom, 16)
-                .refreshable {
-                    Task {
-                        await store.refresh()
-                    }
-                }
-                .onChange(of: searchText, { oldValue, newValue in
-                    Task {
-                        await store.search(newValue)
-                    }
-                })
-                .task {
-                    await store.load()
-                }
+                .toolbar(removing: .sidebarToggle)
+                .navigationSplitViewColumnWidth(min: 330, ideal: 350, max: 400)
             }
-            .toolbar(removing: .sidebarToggle)
-            .navigationSplitViewColumnWidth(min: 330, ideal: 350, max: 400)
             
         } detail: {
             if let selectedCoinID, let coin = store.coinMeta[selectedCoinID] {
@@ -103,8 +101,8 @@ struct MarketView: View {
     MarketView(
         coinService: UpBitAPIService(),
         tickerService: UpbitTickerService(client:
-                                          ReconnectableWebSocketClient {
-                                          BaseWebSocketClient(url: URL(string: "wss://api.upbit.com/websocket/v1")!)
-                                          })
+                                            ReconnectableWebSocketClient {
+                                                BaseWebSocketClient(url: URL(string: "wss://api.upbit.com/websocket/v1")!)
+                                            })
     )
 }
