96f13ab9979ab2233a91f488d3bbaac4318d9fbc
kitcat-s <kitcat92.seo@gmail.com>
2025-08-28 11:06:25 +0900

    design: 코인 썸네일에서 배경색 + 테두리 제거하기

--- AIProject/AIProject/Features/Base/CachedAsyncImage.swift
+++ AIProject/AIProject/Features/Base/CachedAsyncImage.swift
@@ -33,7 +33,6 @@ struct CachedAsyncImage<Content: View>: View {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFit()
-                    .background(.background)
             } else {
                 placeholder
             }
