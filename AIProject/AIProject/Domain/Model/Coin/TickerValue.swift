2890ff6b3421f0dc0f3d1323446b47eccd1adfa2
kangho Lee <ibcylon@naver.com>
2025-08-28 23:36:51 +0900

    refactor: ticker helper computed property
    (cherry picked from commit 0cbebcd084d528848d2722fc42c6b81e94df86db)

--- AIProject/AIProject/Domain/Model/Coin/TickerValue.swift
+++ AIProject/AIProject/Domain/Model/Coin/TickerValue.swift
@@ -27,3 +27,25 @@ extension TickerValue: Equatable {
         lhs.id == rhs.id && lhs.price == rhs.price && lhs.rate == rhs.rate
     }
 }
+
+extension TickerValue {
+    private var code: String {
+        switch change {
+        case .rise: return "▲"
+        case .even: return ""
+        case .fall: return "▼"
+        }
+    }
+    
+    var formatedRate: String {
+        code + rate.formatted(.percent.precision(.fractionLength(2)))
+    }
+    
+    var formatedPrice: String {
+        price.formatted(.number) + "원"
+    }
+    
+    var formatedVolume: String {
+        volume.formatMillion
+    }
+}
