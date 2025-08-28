f57412be08a085a912aa8492c12791a1238f0b68
kitcat-s <kitcat92.seo@gmail.com>
2025-08-28 14:39:44 +0900

    design: 차트 미리보기 디자인 변경하기

--- AIProject/AIProject/Features/MyPage/View/Theme/ThemeView.swift
+++ AIProject/AIProject/Features/MyPage/View/Theme/ThemeView.swift
@@ -13,50 +13,41 @@ struct ThemeView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(spacing: 0) {
-            HeaderView(heading: "테마 변경", showBackButton: true) {
-                dismiss()
-            }
-            
-            SubheaderView(subheading: "차트 색상 변경")
-                .padding(.bottom, 20)
-            
-            VStack(spacing: 16) {
-                ForEach(Theme.allCases, id: \.self) { theme in
-                    ThemeRow(
-                        title: theme.displayName,
-                        theme: theme,
-                        isSelected: themeManager.selectedTheme == theme
-                    ) {
-                        themeManager.selectedTheme = theme
+        ScrollView {
+            VStack(spacing: 0) {
+                HeaderView(heading: "테마 변경", showBackButton: true) {
+                    dismiss()
+                    print("Back")
+                }
+                
+                SubheaderView(subheading: "차트 색상 변경")
+                    .padding(.bottom, 16)
+                
+                VStack(spacing: 16) {
+                    ForEach(Theme.allCases, id: \.self) { theme in
+                        ThemeRow(
+                            title: theme.displayName,
+                            theme: theme,
+                            isSelected: themeManager.selectedTheme == theme
+                        ) {
+                            themeManager.selectedTheme = theme
+                        }
                     }
                 }
+                .padding(.horizontal, 16)
+                .animation(.snappy, value: themeManager.selectedTheme)
+                .buttonStyle(.plain)
             }
-            .padding(.horizontal, 16)
-            .animation(.snappy, value: themeManager.selectedTheme)
-            .buttonStyle(.plain)
-            
-            Spacer(minLength: 0)
+            .padding(.bottom, 40)
             
             /// 미리보기 차트 섹션
-            VStack(alignment: .leading, spacing: 8) {
-                Label {
-                    Text("색상을 변경하면 아래 미리보기 색상이 변경됩니다.")
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
-                } icon: {
-                    Image(systemName: "chart.xyaxis.line") // 또는 "waveform.path.ecgscope"로 골라봤습니다.
-                        .font(.caption)
-                        .symbolRenderingMode(.hierarchical)
-                        .foregroundStyle(.secondary)
-                }
-                .padding(.horizontal, 16)
+            VStack(alignment: .leading, spacing: 16) {
+                SubheaderView(imageName: "chart.xyaxis.line", subheading: "차트 색상 미리보기", imageColor: .aiCoLabelSecondary)
+                    .padding(.bottom, 4)
                 
                 CandlestickPreviewView()
             }
             .transition(.opacity.combined(with: .scale))
-            
-            Spacer(minLength: 0)
         }
         .navigationBarBackButtonHidden()
         .interactiveSwipeBackEnabled()
