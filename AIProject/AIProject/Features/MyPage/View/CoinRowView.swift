8ca3fc43115ed2c56a0c7d8238a56f99b07115cb
kangho Lee <ibcylon@naver.com>
2025-08-29 17:50:03 +0900

    feat: bookmark coin thumbnail
    (cherry picked from commit 4319fb869a4023290d04f072b28c156b6771c3b5)
    (cherry picked from commit 8b93ac1967d7654d9881cadcb1e0efc0b535f97f)

--- AIProject/AIProject/Features/MyPage/View/CoinRowView.swift
+++ AIProject/AIProject/Features/MyPage/View/CoinRowView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct CoinRowView: View {
     let coin: BookmarkEntity
-    let imageURL: URL?
+    let prefetched: UIImage?
     let onDelete: (BookmarkEntity) -> Void
 
     let size: CGFloat = 30
 
     var body: some View {
         HStack(spacing: 12) {
-            CoinView(symbol: coin.coinSymbol, size: size)
+            CoinView(symbol: coin.coinSymbol, size: size, prefetched: prefetched)
 
            	HStack(spacing: 8) {
                 Text(coin.coinKoreanName)
