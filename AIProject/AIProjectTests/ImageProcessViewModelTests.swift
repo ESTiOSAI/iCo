5bcf871ac0c03b2fad118bfd88198271d52318b1
kitcat-s <kitcat92.seo@gmail.com>
2025-09-01 01:11:29 +0900

    fix: 테스트 코드에 API키 검증 단계 추가하기

--- AIProject/AIProjectTests/ImageProcessViewModelTests.swift
+++ AIProject/AIProjectTests/ImageProcessViewModelTests.swift
@@ -21,7 +21,9 @@ final class ImageProcessViewModelTests: XCTestCase {
     }
     
     override func tearDown() async throws {
-        await sut.cancelTask()
+        if sut != nil {
+            await sut.cancelTask()
+        }
         mockCoinList = nil
         sut = nil
         mockImage = nil
@@ -55,6 +57,12 @@ final class ImageProcessViewModelTests: XCTestCase {
     
     // Alan에게서 코인 이름을 받아오지 못했을 때 알맞은 에러를 반환하는지?
     func testImageProcessViewModel_whenNoConvertedSymbol_terminateWithError() async throws {
+        // Alan API키를 검증하고, 키가 없을 시에는 테스트 건너뛰기
+        guard let apiKey = Bundle.main.infoDictionary?["ALAN_API_KEY_COIN_ID_EXTRACTION"] as? String,
+              !apiKey.isEmpty else {
+            throw XCTSkip("테스트에 필요한 Alan API 키를 읽어오지 못함")
+        }
+        
         // Given
         mockImage = ImageProcessTestHelpers.createTestImage(with: "bittcoin batcoin")
         
