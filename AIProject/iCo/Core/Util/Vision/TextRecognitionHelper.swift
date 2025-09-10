fbf6e854571cc47dc1c02fa278d79eea9efcb9a5
kitcat <kitcat92.seo@gmail.com>
2025-09-10 18:13:47 +0900

    refactor: Continuation 코드 가독성 개선하기

--- AIProject/iCo/Core/Util/Vision/TextRecognitionHelper.swift
+++ AIProject/iCo/Core/Util/Vision/TextRecognitionHelper.swift
@@ -22,21 +22,25 @@ final class TextRecognitionHelper {
         
         return try await withCheckedThrowingContinuation { continuation in
             let request = VNRecognizeTextRequest { [weak self] request, error in
+                // DataRace로 인한 참조 해제가 발생하면 안전하게 종료하기
                 guard self != nil else {
                     continuation.resume(throwing: ImageProcessError.unknownVisionError)
                     return
                 }
                 
+                // 메모리 부족 등으로 Vision 처리 자체가 실패하면 안전하게 종료하기
+                guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                    continuation.resume(throwing: ImageProcessError.unknownVisionError)
+                    return
+                }
+                
+                // continuation에서 에러를 반환하면 안전하게 종료하기
                 if let error {
                     continuation.resume(throwing: error)
                     return
                 }
                 
-                guard let observations = request.results as? [VNRecognizedTextObservation] else {
-                    continuation.resume(returning: [])
-                    return
-                }
-                
+                // 결과 Parsing하기
                 let results = observations.compactMap { $0.topCandidates(1).first?.string }
                 continuation.resume(returning: results)
             }
