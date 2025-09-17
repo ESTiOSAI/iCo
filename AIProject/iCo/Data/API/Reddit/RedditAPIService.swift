ad2e87462a224864d1246433a33fd17d7d4316a8
kanghun1121 <kanghun1121@naver.com>
2025-09-17 14:55:54 +0900

    refactor: Reddit API Service 로직 수정

--- AIProject/iCo/Data/API/Reddit/RedditAPIService.swift
+++ AIProject/iCo/Data/API/Reddit/RedditAPIService.swift
@@ -23,7 +23,6 @@ extension RedditEndpoint: Requestable {
 /// 레딧 API 관련 서비스를 제공합니다.
 final class RedditAPIService: CommunityProvider {
     private let network: NetworkClient
-    private let endpoint: String = "https://oauth.reddit.com/r/cryptocurrency/top.json?t=day&limit=5"
     
     init(networkClient: NetworkClient = .init()) {
         self.network = networkClient
@@ -32,10 +31,8 @@ final class RedditAPIService: CommunityProvider {
     /// Reddit의 r/cryptocurrency 서브레딧에서 인기 게시글 데이터를 가져옵니다.
     /// - Returns: 결과는 배열로 반환되며, 각 요소는 하나의 게시글 정보를 포함합니다.
     func fetchData() async throws -> [RedditDTO.RedditResponseDTO.RedditPostDTO] {
-        let urlString = "\(endpoint)"
-        guard let url = URL(string: urlString) else { throw NetworkError.invalidURL }
-        let redditDTO: RedditDTO = try await network.request(url: url)
-        
+        let urlRequest = try RedditEndpoint.main.makeURLrequest()
+        let redditDTO: RedditDTO = try await network.request(for: urlRequest)
         return redditDTO.data.children
     }
 }
