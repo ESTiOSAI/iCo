c0924bbd02e595e454f06c0f99745311a5ab2034
kanghun1121 <kanghun1121@naver.com>
2025-09-17 14:46:32 +0900

    refactor: 공포탐욕지수 API Service 로직 수정

--- AIProject/iCo/Data/API/FearGreed/FearGreedAPIService.swift
+++ AIProject/iCo/Data/API/FearGreed/FearGreedAPIService.swift
@@ -22,8 +22,7 @@ extension FearGreedEndpoint: Requestable {
 
 /// 공포 탐욕 지수 API 관련 서비스를 제공합니다.
 final class FearGreedAPIService: FearGreedProvider {
-    private let network: NetworkClient
-    private let endpoint: String = "https://api.alternative.me/fng/"
+    let network: NetworkClient
 
     init(networkClient: NetworkClient = .init()) {
         self.network = networkClient
@@ -32,10 +31,9 @@ final class FearGreedAPIService: FearGreedProvider {
     /// Alternative의 공포 탐욕 지수 데이터를 가져옵니다.
     /// - Returns: 공포 탐욕 지수, 상태 정보
     func fetchData() async throws -> [FearGreedIndexDTO] {
-        let urlString = "\(endpoint)"
-        guard let url = URL(string: urlString) else { throw NetworkError.invalidURL }
-        let fearGreedResponseDTO: FearGreedResponseDTO = try await network.request(url: url)
-
+        let urlRequest = try FearGreedEndpoint.main.makeURLrequest()
+        let fearGreedResponseDTO: FearGreedResponseDTO = try await network.request(for: urlRequest)
         return fearGreedResponseDTO.data
     }
 }
+
