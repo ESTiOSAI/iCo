6afeee699fb08a6f37405bd82ca0d07fde9106cb
현진 <guswlsrlacl@naver.com>
2025-09-18 15:19:25 +0900

    feat: coinWidget 추가

---
+++ AIProject/iCOWidget/iCOWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  iCOWidgetBundle.swift
+//  iCOWidget
+//
+//  Created by 백현진 on 9/18/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct iCOWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        iCOWidget()
+    }
+}
