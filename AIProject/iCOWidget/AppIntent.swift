f7034f38a479738fbfc26e9f8009e53cf46de622
현진 <guswlsrlacl@naver.com>
2025-09-18 15:19:25 +0900

    feat: coinWidget 추가

---
+++ AIProject/iCOWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  iCOWidget
+//
+//  Created by 백현진 on 9/18/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
