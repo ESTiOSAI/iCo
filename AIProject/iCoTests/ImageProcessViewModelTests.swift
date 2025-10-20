0974cac6a84c37dbdbd2e88211eacc2b4abe379d
kitcat-s <kitcat92.seo@gmail.com>
2025-10-20 15:50:38 +0900

    feat: 테스트 코드를 최신 로직에 맞게 업데이트하기

--- AIProject/iCoTests/ImageProcessViewModelTests.swift
+++ AIProject/iCoTests/ImageProcessViewModelTests.swift
@@ -33,7 +33,7 @@ final class ImageProcessViewModelTests: XCTestCase {
     // 이미지에서 글자를 찾지 못했을 때 알맞은 에러를 반환하는지?
     func testImageProcessViewModel_whenNoTextIsReturned_terminateWithError() async throws {
         // Given
-        mockImage = ImageProcessTestHelpers.createTestImage(with: "").cgImage
+        mockImage = ImageProcessTestHelpers.createTestImage(with: "")
         
         // When
         sut = ImageProcessViewModel()
@@ -47,42 +47,11 @@ final class ImageProcessViewModelTests: XCTestCase {
         // Then
         XCTAssertFalse(sut.isLoading)
         XCTAssertFalse(sut.showAnalysisResultAlert)
-
+        
         XCTAssertTrue(sut.showErrorMessage)
         XCTAssertNotNil(sut.errorMessage)
         XCTAssertTrue(sut.verifiedCoinList.isEmpty)
         
         XCTAssertEqual(sut.errorMessage, ImageProcessError.noRecognizedText.description)
-    }
-    
-    // Alan에게서 코인 이름을 받아오지 못했을 때 알맞은 에러를 반환하는지?
-    func testImageProcessViewModel_whenNoConvertedSymbol_terminateWithError() async throws {
-        // Alan API키를 검증하고, 키가 없을 시에는 테스트 건너뛰기
-        guard let apiKey = Bundle.main.infoDictionary?["ALAN_API_KEY_COIN_ID_EXTRACTION"] as? String,
-              !apiKey.isEmpty else {
-            throw XCTSkip("테스트에 필요한 Alan API 키를 읽어오지 못함")
-        }
-        
-        // Given
-        mockImage = ImageProcessTestHelpers.createTestImage(with: "bittcoin batcoin").cgImage
-        
-        // When
-        sut = ImageProcessViewModel()
-        sut.coinList = mockCoinList
-        sut.processImage(from: mockImage)
-        
-        if let task = sut.processImageTask {
-            _ = try? await task.value
-        }
-        
-        // Then
-        XCTAssertFalse(sut.isLoading)
-        XCTAssertFalse(sut.showAnalysisResultAlert)
-
-        XCTAssertTrue(sut.showErrorMessage)
-        XCTAssertNotNil(sut.errorMessage)
-        XCTAssertTrue(sut.verifiedCoinList.isEmpty)
-        
-        XCTAssertEqual(sut.errorMessage, ImageProcessError.noExtractedCoinID.description)
     }
 }
--- AIProject/iCoTests/TextRecognitionHelperTests.swift
+++ AIProject/iCoTests/TextRecognitionHelperTests.swift
@@ -1,67 +1,64 @@
-////
-////  TextRecognitionHelperTests.swift
-////  AIProjectTests
-////
-////  Created by Kitcat Seo on 8/15/25.
-////
 //
-//import XCTest
-//import Vision
-//@testable import iCo
+//  TextRecognitionHelperTests.swift
+//  AIProjectTests
 //
-//// MARK: - TextRecognitionHelper 테스트
-//final class TextRecognitionHelperTests: XCTestCase {
-//    var sut: TextRecognitionHelper!
-//    
-//    var mockCoinList: Set<String>!
-//    var mockImage: UIImage!
-//    
-//    override func setUp() async throws {
-//        try await super.setUp()
-//        mockCoinList = ["bitcoin"]
-//    }
-//    
-//    override func tearDown() async throws {
-//        sut = nil
-//        mockCoinList = nil
-//        mockImage = nil
-//        try await super.tearDown()
-//    }
-//    
-//    // test대상_작업_예상결과
-//    // OCR 결과 텍스트에 코인 이름이 포함돼있을 때 결과를 정상적으로 반환하는지?
-//    func testHandleOCR_whenTextContainsCoinNames_returnsCoinList() async throws {
-//        // Given
-//        mockImage = ImageProcessTestHelpers.createTestImage(with: "sunrise maple orbit bitcoin velvet")
-//        sut = TextRecognitionHelper(
-//            image: mockImage,
-//            coinNames: mockCoinList
-//        )
-//        
-//        // When
-//        let raw = try await sut.handleOCR()
-//        let results = raw.first!.components(separatedBy: " ")
-//        
-//        // Then
-//        XCTAssertFalse(results.isEmpty)
-//        XCTAssertTrue(results.contains("bitcoin"))
-//    }
-//    
-//    // OCR 결과 텍스트 중 코인 이름이 아닌 문자열에 정상적으로 마스킹이 실행되는지?
-//    func testHandleOCR_whenTextContainsNonCoinNamesOnly_returnsMaskingCharactorsOnly() async throws {
-//        // Given
-//        mockImage = ImageProcessTestHelpers.createTestImage(with: "bittcoin 배트코인")
-//        sut = TextRecognitionHelper(
-//            image: mockImage,
-//            coinNames: mockCoinList
-//        )
-//        
-//        // When
-//        let ocrResult = try await sut.handleOCR()
-//        let allCharacters = ocrResult.joined()
-//        
-//        // Then
-//        let maskingCharacters: Set<Character> = ["*", " "]
-//        XCTAssertTrue(allCharacters.allSatisfy { maskingCharacters.contains($0) })
-//    }
-//}
+//  Created by Kitcat Seo on 8/15/25.
+//
+
+import XCTest
+import Vision
+@testable import iCo
+
+// MARK: - TextRecognitionHelper 테스트
+final class TextRecognitionHelperTests: XCTestCase {
+    var sut: TextRecognitionHelper!
+    
+    var mockCoinList: Set<String>!
+    var mockImage: CGImage!
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        mockCoinList = ["bitcoin"]
+    }
+    
+    override func tearDown() async throws {
+        sut = nil
+        mockCoinList = nil
+        mockImage = nil
+        try await super.tearDown()
+    }
+    
+    // OCR 결과 텍스트에 코인 이름이 포함돼있을 때 결과를 정상적으로 반환하는지?
+    func testHandleOCR_whenTextContainsCoinNames_returnsCoinList() async throws {
+        // Given
+        guard let mockImage = ImageProcessTestHelpers.createTestImage(with: "sunrise maple orbit bitcoin velvet") else {
+            throw XCTSkip("이미지가 정상적으로 생성되지 않음")
+        }
+        sut = TextRecognitionHelper()
+        
+        // When
+        let raw = try await sut.handleOCR(from: mockImage, with: mockCoinList)
+        let results = raw.first!.components(separatedBy: " ")
+        
+        // Then
+        XCTAssertFalse(results.isEmpty)
+        XCTAssertTrue(results.contains("bitcoin"))
+    }
+    
+    // OCR 결과 텍스트 중 코인 이름이 아닌 문자열에 정상적으로 마스킹이 실행되는지?
+    func testHandleOCR_whenTextContainsNonCoinNamesOnly_returnsMaskingCharactorsOnly() async throws {
+        // Given
+        guard let mockImage = ImageProcessTestHelpers.createTestImage(with: "bittcoin 배트코인") else {
+            throw XCTSkip("이미지가 정상적으로 생성되지 않음")
+        }
+        sut = TextRecognitionHelper()
+        
+        // When
+        let ocrResult = try await sut.handleOCR(from: mockImage, with: mockCoinList)
+        let allCharacters = ocrResult.joined()
+        
+        // Then
+        let maskingCharacters: Set<Character> = ["*", " "]
+        XCTAssertTrue(allCharacters.allSatisfy { maskingCharacters.contains($0) })
+    }
+}
