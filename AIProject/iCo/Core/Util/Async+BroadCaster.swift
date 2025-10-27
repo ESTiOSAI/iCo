f548137a871c64cf6198fac777604a358579138a
kangho <ibcylon@naver.com>
2025-10-27 11:42:47 +0900

    comment: timeout, broadcaster 주석 작업

--- AIProject/iCo/Core/Util/Async+BroadCaster.swift
+++ AIProject/iCo/Core/Util/Async+BroadCaster.swift
@@ -12,15 +12,25 @@ actor BroadCaster {
     private init() {}
 }
 
+
+/// AsyncStream의 다중 소비를 위해 만든 객체로 스트림 전파 수행
 public class AsyncStreamBroadcaster<Element> {
+    
+    /// 구독할 continuation 값들
     private var continuations: [AsyncStream<Element>.Continuation] = []
 
+    
+    /// 구독 메서드로 stream을 반환
+    /// - Returns: stream 반환
     public func stream() -> AsyncStream<Element> {
         AsyncStream { continuation in
             continuations.append(continuation)
         }
     }
 
+    
+    /// 전파할 메세지를 전송하고 구독자들에게 전파
+    /// - Parameter element: 메세지
     @BroadCaster
     public func send(_ element: Element) async {
         for c in continuations {
@@ -28,6 +38,8 @@ public class AsyncStreamBroadcaster<Element> {
         }
     }
 
+    
+    /// 구독을 해제하고 스트림을 종료함
     public func finish() {
         for c in continuations {
             c.finish()
--- AIProject/iCo/Core/Util/Async+Timeout.swift
+++ AIProject/iCo/Core/Util/Async+Timeout.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+
+/// 두 클로저 중 먼저 완료되는 클로저만 실행하고 나머지는 취소
+/// - Parameters:
+/// - Returns: 먼저 완료되는 클로저 실행
 public func race<T>(
     _ lhs: sending @escaping () async throws -> T,
     _ rhs: sending @escaping () async throws -> T
@@ -21,6 +25,13 @@ public func race<T>(
     }
 }
 
+
+/// async 작업에 timeout을 줌
+/// - Parameters:
+///   - action: 수행할 비동기 클로저
+///   - timeout: 타임아웃 duration
+/// - Throws: 타임아웃 에러 또는 작업에서 발생한 에러
+/// - Returns: 비동기 작업 결과값
 public func performWithTimeout<T>(
     _ action: sending @escaping () async throws -> T,
     at timeout: Duration
