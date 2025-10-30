83723f15a55714b2c6771f4832e65aa8327c3b6b
kanghun1121 <kanghun1121@naver.com>
2025-10-30 20:03:35 +0900

    fix: 브로드캐스터 메모리 누수 해결

--- AIProject/iCo/Core/Util/Async+BroadCaster.swift
+++ AIProject/iCo/Core/Util/Async+BroadCaster.swift
@@ -5,6 +5,8 @@
 //  Created by 강대훈 on 10/26/25.
 //
 
+import Foundation
+
 @globalActor
 actor BroadCaster {
     static let shared = BroadCaster()
@@ -12,19 +14,24 @@ actor BroadCaster {
     private init() {}
 }
 
-
 /// AsyncStream의 다중 소비를 위해 만든 객체로 스트림 전파 수행
 public class AsyncStreamBroadcaster<Element> {
     
     /// 구독할 continuation 값들
-    private var continuations: [AsyncStream<Element>.Continuation] = []
-
+    private var continuations: [UUID: AsyncStream<Element>.Continuation] = [:]
     
     /// 구독 메서드로 stream을 반환
     /// - Returns: stream 반환
     public func stream() -> AsyncStream<Element> {
-        AsyncStream { continuation in
-            continuations.append(continuation)
+        let id = UUID()
+        
+        return AsyncStream<Element> { continuation in
+            continuation.onTermination = { [weak self] _ in
+                guard let self = self else { return }
+                self.finish(id: id)
+            }
+            
+            continuations[id] = continuation
         }
     }
 
@@ -33,7 +40,7 @@ public class AsyncStreamBroadcaster<Element> {
     /// - Parameter element: 메세지
     @BroadCaster
     public func send(_ element: Element) async {
-        for c in continuations {
+        for (_, c) in continuations {
             c.yield(element)
         }
     }
@@ -41,8 +48,12 @@ public class AsyncStreamBroadcaster<Element> {
     
     /// 구독을 해제하고 스트림을 종료함
     public func finish() {
-        for c in continuations {
+        for (_, c) in continuations {
             c.finish()
         }
+    }
+    
+    private func finish(id: UUID) {
+        continuations[id] = nil
     }
 }
