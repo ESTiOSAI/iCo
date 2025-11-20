ba7fb91b1838ce8c33d9dae78b8c7564d93298c4
jihyeonjjang <jihyeon9975@gmail.com>
2025-11-20 13:33:20 +0900

    fix: 커뮤니티 분위기 디코딩 에러로 프롬프트 수정

--- AIProject/iCo/Domain/Model/Common/Prompt.swift
+++ AIProject/iCo/Domain/Model/Common/Prompt.swift
@@ -85,7 +85,7 @@ enum Prompt {
                 let summary: String
             }
             
-            커뮤니티 분위기(호재, 악재, 중립)와 그렇게 평가한 이유를 한글로 200자 이상으로 요약해 위 형식으로 작성해서 JSON으로 제공 (답변은 한글, 마크다운 금지, 출처 제외)
+            커뮤니티 분위기(호재, 악재, 중립)와 그렇게 평가한 이유를 한글로 200자 이상으로 요약해 위 InsightDTO로 디코딩 할 수 있도록 작성해서 JSON으로 제공 (답변은 한글, 종결 어미는 '-ㅂ니다', 마크다운 금지, 출처 제외)
             """
         case .generateBookmarkBriefing(let importance, let bookmarks):
             """
